638aa2c5ae8371ce42352a40da81301ec483540b
valeriarojasj <valeriarojasj@gmail.com>
2021-01-09 20:06:42 -0600

    publique vendor/laravel.jetsream/resources/views en resources/views/vendor/jetstream para actualizar los logos y los actualicé

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,8 @@
     
 <header>
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href=""><img src="img/logo-fyg.png" height="50px" alt=""></a>
+      <a class="navbar-brand" href="/"><img src="img/logo-fyg.png" style="
+        height: 50px;" alt=""></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
