12c7587aa369a0802479f8b57fcc5f3cc0eef176
lautaro valle lopez <lautarovallelopez@gmail.com>
2021-01-14 02:51:25 -0300

    stepform, botones next y back funcionan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -206,7 +206,61 @@
           <br>
           </div>
 </div>
+<button class="px-4 py-2 font-bold text-gray-500 bg-transparent border border-gray-500 rounded-full modal-open hover:border-indigo-500 hover:text-indigo-500">Postularme</button>
+
+
+
+<div class="fixed top-0 left-0 flex items-center justify-center w-full h-full opacity-0 pointer-events-none modal">
+  <!--Modal Overlay-->
+  <div class="absolute w-full h-full bg-gray-900 opacity-50 modal-overlay"></div>
+  <!--Cierre Modal Overlay-->
+  <!--Contenedor del modal-->
+  <div class="z-50 w-11/12 mx-auto overflow-y-auto bg-white rounded shadow-lg modal-container md:max-w-md">
+    <div class="absolute top-0 right-0 z-50 flex flex-col items-center mt-4 mr-4 text-sm text-white cursor-pointer modal-close">
+      <svg class="text-white fill-current" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+      </svg>
+      <span class="text-sm"></span>
+    </div>
+
+    <!-- Contenido del modal (Title y Body)-->
+    <div class="px-6 py-4 text-left modal-content">
+      <!--Title-->
+      <div class="flex items-center justify-between pb-3">
+        <p class="text-2xl font-bold">Preguntas</p>
+        <div class="z-50 cursor-pointer modal-close">
+          <svg class="text-black fill-current" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+            <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+          </svg>
+        </div>
+      </div>
+      <!--Cierre del Title-->
+
+
 <livewire:formulario />
+
+
+
+      </div>
+  </div>
+  <!--Cierre del Contenedor del Modal-->
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </section>
 
 
