eddb467e96a41deea9f9242f049a7886904bdafb
lautaro valle lopez <lautarovallelopez@gmail.com>
2021-01-16 20:19:04 -0300

    search input funciona, el paginador tambien

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -23,10 +23,10 @@ class CandidateController extends Controller
         $columnToOrder = $order[0]['column'];
         $typeOfOrder = $order[0]['dir'];
         $columnName = $columns[$columnToOrder]['data'];
-        
-        $records = Candidate::all();
+      
+        $searchRecords = $this->searchCandidates($columnName, $typeOfOrder, $start, $length, $search['value']);
         $data_arr = array();
-        foreach($records as $record){
+        foreach($searchRecords as $record){
             $data_arr[] = array(
                 "id" => $record->id,
                 "fullName" => $record->fullName,
@@ -45,22 +45,29 @@ class CandidateController extends Controller
             );
         }
         $response = array(
-            "candidates"=>Schema::getColumnListing('candidates'),
             "draw" => intval($draw),
-            "search" => $search,
-            "start" => $start,
-            "length" => $length,
-            "order" => $order,
-            "columns" => $columns,
             "iTotalRecords" => $this->countAllRows(),
-            "iTotalDisplayRecords" => count($records),
-            "aaData" => $records
+            "iTotalDisplayRecords" => $this->countSearchRows($columnName, $typeOfOrder, $start, $length, $search['value']),
+            "aaData" => $data_arr
         );
         echo json_encode($response);
         exit;
     }
+
+
+
     public function countAllRows(){
         return Candidate::select('count(*) as allcount')->count();
+    }
+
+    public function countSearchRows($column, $order, $start, $length, $valueToSearch){
+        $columnsNames = Schema::getColumnListing('candidates');
+        $records = Candidate::select('*')
+                    ->where('id', 'like', '%'.$valueToSearch.'%');
+        for ($i = 1; $i < count($columnsNames); ++$i){
+            $records->orWhere($columnsNames[$i], 'like', '%'.$valueToSearch.'%');
+        }
+        return $records->count();
     }
 
 
@@ -71,10 +78,10 @@ class CandidateController extends Controller
         for ($i = 1; $i < count($columnsNames); ++$i){
             $records->orWhere($columnsNames[$i], 'like', '%'.$valueToSearch.'%');
         }
-        /*$records->orderby($column, $order)
+        return $records->orderby($column, $order)
             ->skip($start)
             ->take($length)
-            ->get();*/
-        return $records->get();
+            ->get();
     }
+
 }
