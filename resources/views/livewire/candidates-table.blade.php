4b474fe40c3e9a04cab87ea6df6b5f930e9f329f
lautaro valle lopez <lautarovallelopez@gmail.com>
2021-01-19 20:18:26 -0300

    boton de excel funcionando, despues de arreglos de filtros

--- resources/views/livewire/candidates-table.blade.php
+++ resources/views/livewire/candidates-table.blade.php
@@ -60,7 +60,20 @@
             <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
             <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
             <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.22/b-1.6.4/b-flash-1.6.4/b-html5-1.6.4/b-print-1.6.4/datatables.min.js"></script>
+            <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+            <script type="text/javascript" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+            <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+            <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
             
+
+
+
+
+
+
             <script>
                 $(document).ready(function () {
                     $.ajaxSetup({
@@ -95,12 +108,15 @@
                         ],
                         responsive: true,
                         autoWidth : false,
-                        dom: 'Blfrtip',
                         "scrollX": true,
                         "scrollY": true,
                         scrollCollapse: true,
+                        dom: 'Bfrtip',
                         buttons: [
-                            'copy', 'excel', 'pdf'
+                            'copyHtml5',
+                            'excelHtml5',
+                            'csvHtml5',
+                            'pdfHtml5'
                         ],
                         language:{
                             paginate : {
