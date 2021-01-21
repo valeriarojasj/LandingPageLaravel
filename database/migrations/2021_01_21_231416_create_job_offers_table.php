617bd015124064f7995c6951d63552ecae8f608e
valeriarojasj <valeriarojasj@gmail.com>
2021-01-21 20:27:34 -0300

    cree migracion de job_offers

---
+++ database/migrations/2021_01_21_231416_create_job_offers_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_offers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('job_title');
+            $table->string('company_type');
+            $table->string('job_location');
+            $table->string('open_question_1');
+            $table->string('open_question_2');
+            $table->string('multiple_choice_question_1');
+            $table->string('multiple_choice1_option_1');
+            $table->string('multiple_choice1_option_2');
+            $table->string('multiple_choice1_option_3');
+            $table->string('multiple_choice_question_2');
+            $table->string('multiple_choice2_option_1');
+            $table->string('multiple_choice2_option_2');
+            $table->string('multiple_choice2_option_3');
+            $table->string('checkbox_question_1');
+            $table->string('checkbox1_option_1');
+            $table->string('checkbox1_option_2');
+            $table->string('checkbox1_option_3');
+            $table->string('checkbox_question_2');
+            $table->string('checkbox2_option_1');
+            $table->string('checkbox2_option_2');
+            $table->string('checkbox2_option_3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_offers');
+    }
+}
