2c752bb24de9bdb1c15c1fc2e7e5b8ae998a6fa3
valeriarojasj <valeriarojasj@gmail.com>
2021-01-21 19:12:51 -0300

    solucione problema de modal que ocultaba otros elementos aun sin estar activo con toggle hidden en modal-container

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,11 +16,16 @@
     <style>
       .modal {
         transition: opacity 0.25s ease;
+        
+        
       }
       body.modal-active {
         overflow-x: hidden;
         overflow-y: visible !important;
       }
+
+      
+
     </style>
  {{-- end of Tailwind modal head --}}
     <script src="https://kit.fontawesome.com/b67648323f.js" crossorigin="anonymous"></script>
@@ -222,7 +227,7 @@
       <br>
     </div>
 
-     <div class="grid max-w-screen-xl grid-cols-1 gap-4 px-4 mx-auto lg:grid-cols-3">
+     <div class="grid max-w-screen-xl grid-cols-3 gap-4 px-4 mx-auto">
        <div class="px-4 py-3 rounded-xl d-flex flex-column tarjetaBusqueda">
          <h5 class="tituloBusqueda">Programador PHP</h5>
          <p class="empresaBusqueda">Empresa Farmacéutica</p>
@@ -313,7 +318,7 @@
   <div class="absolute w-full h-full bg-gray-900 opacity-50 modal-overlay"></div>
   <!--Cierre Modal Overlay-->
   <!--Contenedor del modal-->
-  <div class="z-50 w-11/12 mx-auto overflow-y-auto bg-white rounded shadow-lg modal-container md:max-w-md">
+  <div class="z-50 hidden w-11/12 mx-auto overflow-y-auto bg-white rounded shadow-lg modal-container md:max-w-md">
     <div class="absolute top-0 right-0 z-50 flex flex-col items-center mt-4 mr-4 text-sm text-white cursor-pointer modal-close">
       <svg class="text-white fill-current" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
         <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
@@ -448,9 +453,12 @@
   function toggleModal () {
     const body = document.querySelector('body')
     const modal = document.querySelector('.modal')
+    const modalContainer = document.querySelector('.modal-container')
     modal.classList.toggle('opacity-0')
     modal.classList.toggle('pointer-events-none')
     body.classList.toggle('modal-active')
+    modalContainer.classList.toggle('hidden')
+    
   }
   
    
