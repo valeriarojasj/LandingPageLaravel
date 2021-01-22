a4699673365f6a1a4fe02201d96de2916dc5050c
valeriarojasj <valeriarojasj@gmail.com>
2021-01-22 17:23:53 -0300

    añadi Búsquedas Laborales al menu de navegacion del Dashboard, con su ruta, y su componente Livewire JobOpenings

---
+++ resources/views/livewire/job-openings.blade.php
@@ -0,0 +1,10 @@
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            Búsquedas Laborales
+        </h2>
+    </x-slot>
+   
+    
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/candidatos', [CandidateCo
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/job-openings', function () {
+    return view('livewire.job-openings');
+})->name('job-openings');
