02d10d0ce58268198bf8dc6311383cbde5018be7
valeriarojasj <valeriarojasj@gmail.com>
2021-02-17 17:36:39 -0300

    tabla de usuarios en vista users-index.blade.php

--- resources/views/livewire/admin/users-index.blade.php
+++ resources/views/livewire/admin/users-index.blade.php
@@ -11,7 +11,49 @@
                 <!--Card-->
                 <div id='users-roles' class="mt-6 bg-white rounded shadow lg:mt-0">
 
-                
+                <div class="overflow-x-auto sm:-mx-6 lg:-mx-8" >  <!--este div hace que haga scroll la tabla -->
+                    <div class="inline-block min-w-full py-2 space-between sm:px-6 lg:px-8">
+                      <div class="mb-3 border-b border-gray-200 shadow align-left sm:rounded-lg">
+                        <table class="table mb-0" id="editableTable">
+                          <thead class="bg-blue-100">
+                            <tr>
+                              <th scope="col" class="px-6 py-3 text-xs font-bold tracking-wider text-center text-blue-700 uppercase align-text-top hard_left">
+                                Opciones de Edición
+                              </th>
+                              <th scope="col" class="px-6 py-3 text-xs font-bold tracking-wider text-center text-blue-700 uppercase align-text-top next_left">
+                                ID
+                              </th>
+                              <th scope="col" class="px-6 py-3 text-xs font-bold tracking-wider text-center text-blue-700 uppercase align-text-top">
+                                Nombre
+                              </th>
+                              <th scope="col" class="px-6 py-3 text-xs font-bold tracking-wider text-center text-blue-700 uppercase align-text-top">
+                                Email
+                              </th>
+                              <th scope="col" class="px-6 py-3 text-xs font-bold tracking-wider text-center text-blue-700 uppercase align-text-top">
+                                Status
+                              </th>
+                              <th scope="col" class="px-6 py-3 text-xs font-bold tracking-wider text-center text-blue-700 uppercase align-text-top">
+                                Rol
+                              </th>
+                              <th scope="col" class="px-6 py-3 text-xs font-bold tracking-wider text-center text-blue-700 uppercase align-text-top">
+                                Fecha de Creación
+                              </th>
+                              <th scope="col" class="px-6 py-3 text-xs font-bold tracking-wider text-center text-blue-700 uppercase align-text-top">
+                                Fecha de Actualización
+                              </th>
+                            </tr>
+                          </thead>
+                          <tbody class="bg-white divide-y divide-gray-200">
+                          
+                           
+                          </tbody>
+                        </table>
+                        
+                      </div>
+                      
+                    </div>
+                   
+                  </div>
               
                    
                 </div>
