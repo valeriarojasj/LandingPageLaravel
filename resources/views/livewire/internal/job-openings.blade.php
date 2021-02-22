05e32942ba5287f7f4111baca8ed433892be7883
valeriarojasj <valeriarojasj@gmail.com>
2021-02-22 17:07:06 -0300

    solucione problema de ruta de job-openings.js

--- resources/views/livewire/internal/job-openings.blade.php
+++ resources/views/livewire/internal/job-openings.blade.php
@@ -142,7 +142,7 @@
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
   <!-- <script src="https://cdn.jsdelivr.net/npm/pikaday/pikaday.js"></script> -->
   <script src="https://kit.fontawesome.com/dd0322cf66.js" crossorigin="anonymous"></script>
-  <script type="text/javascript" src="js/job-openings.js"></script>
+  <script type="text/javascript" src="{{ asset('js/job-openings.js') }}"></script>
 </div>
                    
  
