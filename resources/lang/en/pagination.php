86d085af62b15c368893c6d71c4ca04d575a6ca9
valeriarojasj <valeriarojasj@gmail.com>
2021-03-15 21:04:08 -0300

    traduccion de errores

--- resources/lang/en/pagination.php
+++ resources/lang/en/pagination.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'previous' => '&laquo; Previous',
-    'next' => 'Next &raquo;',
+    'previous' => '&laquo; Atrás',
+    'next' => 'Siguiente &raquo;',
 
 ];
--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Tu password ha sido cambiado!',
+    'sent' => 'El link para cambiar tu password ha sido enviado a tu email!',
+    'throttled' => 'Por favor espera antes de volver a intentarlo nuevamente.',
+    'token' => 'Este token para cambiar el password es inválido.',
+    'user' => "No encontramos ningún usuario con ese email.",
 
 ];
